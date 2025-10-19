ff775be6cd602f2adccaaa2b9c8f4fe26bb98782
Samir Buranov <samirhan2706@gmail.com>
2025-10-19 23:03:53 +0500

    hello version

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    const char* lang = "C++";
+    std::cout << "САУ " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+    return 0;
+}
