3fea27443f60e241b18800efa44a3c07829a1737
Samir Buranov <samirhan2706@gmail.com>
2025-10-28 00:53:00 +0500

    База просто

--- main.cpp
+++ main.cpp
@@ -28,9 +28,9 @@ public:
         y = p.y;
     }
 
-    ~Point(){
-        cout << "~Point()" << endl;
-        cout << x << ", " << y << endl;
+    virtual ~Point(){
+        cout << "~Point(): " << x << ", " << y << endl;
+
     }
     void move(int dx, int dy){
         x = x + dx;
@@ -58,21 +58,53 @@ public:
         this->color = color;
     }
 
-    ColoredPoint(const ColoredPoint &p){
-        cout << "ColoredPoint(const ColoredPoint &p)" << endl;
-        x = p.x;
-        y = p.y;
+    ColoredPoint(const ColoredPoint& p) : Point(p) {
+        color = p.color;
+        cout << "ColoredPoint(const ColoredPoint& p)" << endl;
     }
 
     ~ColoredPoint(){
-        cout << "~ColoredPoint()" << endl;
-        cout << x << ", " << y << ", " << color << endl;
+        cout << "~ColoredPoint(): " << x << ", " << y << ", " << color<< endl;
     }
     void change_color(int new_color){
         color = new_color;
     }
 
 };
+
+class Section{
+protected:
+    Point *p1;
+    Point *p2;
+public:
+    Section(){
+        cout << "Section()" << endl;
+        p1 = new Point();
+        p2 = new Point();
+    }
+
+    Section(int x1, int y1, int x2, int y2){
+        cout << "Section(int x1, int y1, int x2, int y2)" << endl;
+        p1 = new Point(x1,y1);
+        p2 = new Point (x2,y2);
+    }
+
+    Section(const Section &s){
+        cout << "Section(const Section &p)" << endl;
+        p1 = new Point(*(s.p1));
+        p2 = new Point(*(s.p2));
+    }
+
+    virtual ~Section(){
+        cout << "~Section()" << endl;
+        delete p1;
+        delete p2;
+
+    }
+
+};
+
+
 
 
 int main() {
@@ -86,10 +118,10 @@ int main() {
 //    }
 //    cout << "\nДинамически" << endl;
 //
-//    Point *p = new Point();
+//    Point *p1 = new Point();
 //    Point *p2 = new Point(30,60);
 //    Point *p3 = new Point(*p2);
-//    delete p;
+//    delete p1;
 //    delete p2;
 //    delete p3;
 
@@ -101,9 +133,12 @@ int main() {
 //    delete p;
 
     // НАСЛЕДОВАНИЕ
-    ColoredPoint *p = new ColoredPoint(1,2,52);
-    delete p;
+//    ColoredPoint *p = new ColoredPoint(1,2,52);
+//    delete p;
 
+    //Колоред поинт как поинт
+//    Point *p = new ColoredPoint(1, 2 ,42);
+//    delete p;
 
 
 
