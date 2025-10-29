5d53bd4b98051df0e53d3b7295847e12e843d0ec
Samir Buranov <samirhan2706@gmail.com>
2025-10-30 02:46:54 +0500

    FINAL

--- main.cpp
+++ main.cpp
@@ -40,6 +40,7 @@ public:
 };
 
 void Point::reset() {
+    cout << "Point reset()" << endl;
     x = 0;
     y = 0;
 }
@@ -69,8 +70,16 @@ public:
     void change_color(int new_color){
         color = new_color;
     }
+    void reset() {
+        cout << "ColoredPoint reset()\n";
+        Point::reset();
+        color = 0;
+    }
+
 
 };
+
+class PoorPoint : public Point{};
 
 class Section{
 protected:
@@ -104,6 +113,39 @@ public:
 
 };
 
+class SectionPP {
+protected:
+    Point p1;
+    Point p2;
+public:
+    SectionPP() {
+        cout << "SectionPP()" << endl;
+        // создаются два Point по умолчанию
+    }
+
+    SectionPP(int x1, int y1, int x2, int y2) {
+        cout << "SectionPP(int x1, int y1, int x2, int y2)" << endl;
+        p1 = Point(x1, y1);
+        p2 = Point(x2, y2);
+    }
+
+    SectionPP(const SectionPP &s) {
+        cout << "SectionPP(const SegmentDirect &s)" << endl;
+        p1 = s.p1;
+        p2 = s.p2;
+    }
+
+    ~SectionPP() {
+        cout << "~SectionPP()" << endl;
+    }
+
+    void move(int dx, int dy) {
+        p1.move(dx, dy);
+        p2.move(dx, dy);
+    }
+};
+
+
 
 
 
@@ -126,9 +168,9 @@ int main() {
 //    delete p3;
 
     //Метод у Point
-    //К полям обратиться не можем, но можем вызвать публичный метод (поля - привате)
+    //К полям обратиться не можем, но можем вызвать публичный метод (поля - протектед)
 //    Point *p = new Point(1,2);
-////    p->reset();
+//    p->reset();
 //    p->move(100,200);
 //    delete p;
 
@@ -141,6 +183,58 @@ int main() {
 //    delete p;
 
 
+    //переопределение род метода в потомке
+//    {
+//
+//        ColoredPoint cp(1,2,7);
+//
+//        cout << "Вызов через ColoredPoint: ";
+//        cp.reset(); // вызовется ColoredPoint::reset()
+//
+//        Point* p = &cp;
+//        cout << "Вызов через Point: ";
+//        p->reset(); //
+//    }
 
+
+    //класс с точками не указателями а прям объектами  vs класс с указателями
+//    cout << "\nSection (c указателями)\n" << endl;
+//    {
+//        Section s1(10, 20, 30, 40);
+//    }
+//
+//    cout << "\nSegmentPP (без указателей)\n" << endl;
+//    {
+//        SectionPP s2(10, 20, 30, 40);
+//    }
+
+
+    //копирование обьекта в объект  и указатель в указатель
+//    cout << "\nКопирование объект в объект" << endl;
+//    {
+//        Point a(1, 2);
+//        Point b;
+//        b = a;
+//        a.move(100, 200);
+//    }
+//
+//    cout << "\nКопирование указатель в указатель" << endl;
+//    {
+//        Point* p1 = new Point(10, 20);
+//        Point* p2 = p1;;
+//        p2->move(50, 50);
+//        delete p1;
+//        delete p2;
+//    }
+//    cout << "Потомок без конструкторов и деструкторов" << endl;
+//    {
+//        PoorPoint pp;
+//    };
+    //что не так со второй командой
+//    cout << "Потеря" << endl;
+//    {
+//        Point p = ColoredPoint(1,2,3); //создается поинт по факту
+//
+//    }
     return 0;
 }
